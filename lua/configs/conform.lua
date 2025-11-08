92e45e5f7e9b23951fe08d9e52531c48abdd07eb
mamangbili <mhabilr30@gmail.com>
2025-11-08 10:39:58 +0700

    feat : confrom detecting clang-format if not fallback to default

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,4 +1,28 @@
+-- check if .clangd-format exist
+local function clangd_format_exists_in_root()
+  local cwd = vim.fn.getcwd()
+  local clangd_format_path = cwd .. "\\.clang-format"
+  local file = io.open(clangd_format_path, "r")
+  print("Checking for .clangd-format at: " .. clangd_format_path .. " - Found: " .. tostring(file ~= nil))
+
+  if file then
+    io.close(file)
+    return {}
+  else
+    return {
+      "--style={BasedOnStyle: Chromium, IndentWidth: 4, AlignConsecutiveDeclarations: true}",
+      "--fallback-style=Google",
+    }
+  end
+end
+
 local options = {
+  formatters = {
+    clang_format = {
+      prepend_args = clangd_format_exists_in_root(),
+    },
+  },
+
   formatters_by_ft = {
     lua = { "stylua" },
     py = { "ast-grep" },
@@ -6,7 +30,8 @@ local options = {
     rust = { "ast-grep" },
     typescript = { "ast-grep" },
     go = { "ast-grep" },
-    cpp = { "clang-format" },
+    -- cpp = { "clang-format" },
+    cpp = { "clang_format" },
     c = { "clangd" },
     jsx = { "ast-grep" },
     elixir = { "mix" },
