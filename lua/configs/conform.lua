bbacc6c93c0fe055fe03519acd5594fd1a2f8f41
mamangbili <mhabilr30@gmail.com>
2025-12-18 08:38:00 +0700

    fix : noice inlay stealing focus in insert mode

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,45 +1,45 @@
 -- check if .clangd-format exist
 local function clangd_format_exists_in_root()
-  local cwd = vim.fn.getcwd()
-  local clangd_format_path = cwd .. "\\.clang-format"
-  local file = io.open(clangd_format_path, "r")
-  -- print("Checking for .clangd-format at: " .. clangd_format_path .. " - Found: " .. tostring(file ~= nil))
+    local cwd = vim.fn.getcwd()
+    local clangd_format_path = cwd .. "\\.clang-format"
+    local file = io.open(clangd_format_path, "r")
+    -- print("Checking for .clangd-format at: " .. clangd_format_path .. " - Found: " .. tostring(file ~= nil))
 
-  if file then
-    io.close(file)
-    return {}
-  else
-    return {
-      "--style={BasedOnStyle: Chromium, IndentWidth: 4, AlignConsecutiveDeclarations: true, SortIncludes: false}",
-      "--fallback-style=Google",
-    }
-  end
+    if file then
+        io.close(file)
+        return {}
+    else
+        return {
+            "--style={BasedOnStyle: Chromium, IndentWidth: 4, AlignConsecutiveDeclarations: true, SortIncludes: false}",
+            "--fallback-style=Google",
+        }
+    end
 end
 
 local options = {
-  formatters = {
-    clang_format = {
-      prepend_args = clangd_format_exists_in_root(),
+    formatters = {
+        clang_format = {
+            prepend_args = clangd_format_exists_in_root(),
+        },
     },
-  },
 
-  formatters_by_ft = {
-    lua = { "stylua" },
-    py = { "ast-grep" },
-    javascript = { "ast-grep" },
-    rust = { "ast-grep" },
-    typescript = { "ast-grep" },
-    go = { "ast-grep" },
-    cpp = { "clang_format" },
-    c = { "clangd" },
-    jsx = { "ast-grep" },
-    elixir = { "mix" },
-    cmake = { "gersemi" },
-    json = { "fixjson" },
-  },
-  default_format_opts = {},
-  -- Set up format-on-save
-  format_on_save = { timeout_ms = 500 },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        py = { "ast-grep" },
+        javascript = { "ast-grep" },
+        rust = { "ast-grep" },
+        typescript = { "ast-grep" },
+        go = { "ast-grep" },
+        cpp = { "clang_format" },
+        c = { "clangd_format" },
+        jsx = { "ast-grep" },
+        elixir = { "mix" },
+        cmake = { "gersemi" },
+        json = { "fixjson" },
+    },
+    default_format_opts = {},
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 500 },
 }
 
 return options
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,9 +1,57 @@
 return {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "rcarriga/nvim-notify",
+    "folke/noice.nvim",
+    -- enabled = false,
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
+    opts = {
+        popupmenu = {
+            enabled = false,
+            backend = "cmp",
         },
-        config = require "configs.noice",
-    }
+        cmdline = {
+            enable = true,
+            format = {
+                lua = {
+                    icon = "", -- ← no icon (was "tolua")
+                    name = "lua", -- keep name for filtering (optional)
+                    pattern = "^lua%s", -- matches ":lua " (case-insensitive by default)
+                },
+            },
+        },
+        lsp = {
+            signature = {
+                enabled = false,
+            },
+            hover = {
+                enabled = false,
+            },
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = false,
+            },
+        },
+        presets = {
+            lsp_doc_border = true, -- Preserve borders
+        },
+        views = {
+            cmdline_popup = {
+                border = {
+                    style = "rounded",
+                    padding = { 1, 2 },
+                },
+            },
+        },
+    },
+    init = function(_, opts)
+        -- require("noice").setup(opts)
+        require("notify").setup {
+            fps = 10,
+            top_down = false,
+            stages = "static",
+        }
+    end,
+}
